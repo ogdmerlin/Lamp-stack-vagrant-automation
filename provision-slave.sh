5ae5819f16824e0b7b30639908f9f6fc9644e854
ogdmerlin <ogdmerlin@gmail.com>
2023-10-13 18:22:20 +0100

    Vagrant Ubuntu Cluster

--- provision-slave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create user 'altschool'
-useradd -m -s /bin/bash altschool
-
-# Copy data from Master to Slave
-scp -o StrictHostKeyChecking=no -i /home/altschool/.ssh/id_rsa -r altschool@192.168.33.10:/mnt/altschool /mnt/altschool/slave
-
