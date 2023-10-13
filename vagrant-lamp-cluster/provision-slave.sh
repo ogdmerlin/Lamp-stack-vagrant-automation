75a33b3b70a65e2aea45e167c749dbf7fefb14fc
ogdmerlin <ogdmerlin@gmail.com>
2023-10-13 19:12:42 +0100

    Slave Node Script Error Handling Was Introduced

--- vagrant-lamp-cluster/provision-slave.sh
+++ vagrant-lamp-cluster/provision-slave.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# Function to handle errors
+handle_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    handle_error "This script must be run as root."
+fi
+
+# Check if required directory exists
+if [ ! -d "/mnt/altschool" ]; then
+    handle_error "/mnt/altschool directory does not exist."
+fi
+
 # Create user 'altschool'
-useradd -m -s /bin/bash altschool
+useradd -m -s /bin/bash altschool || handle_error "Failed to create user altschool."
 
 # Copy data from Master to Slave
-scp -o StrictHostKeyChecking=no -i /home/altschool/.ssh/id_rsa -r altschool@192.168.33.10:/mnt/altschool /mnt/altschool/slave
-
+scp -o StrictHostKeyChecking=no -i /home/altschool/.ssh/id_rsa -r altschool@192.168.33.10:/mnt/altschool /mnt/altschool/slave || handle_error "Failed to copy data."
